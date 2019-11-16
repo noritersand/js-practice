08469f0b6fbbc569261bb62725427773165fbd89
noritersand <noritersand@gmail.com>
2019-11-17 01:44:26 +0900

    하나로 모음

--- test/number/number-format-prototype-extend-test.js
+++ test/number/number-format-prototype-extend-test.js
@@ -10,9 +10,61 @@ Number.prototype.format = function(n, x) {
     var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
     return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
 };
+console.log('format():')
+console.log('- 1234:', (1234).format());           // "1,234"
+console.log('- 1234:', (1234.).format());           // "1,234"
+console.log('- 12345:', (12345.).format(2));         // "12,345.00"
+console.log('- 123456.7:', (123456.7).format(3, 2));    // "12,34,56.700"
+console.log('- 123456.789:', (123456.789).format(2, 4));  // "12,3456.79"
+console.log('\n');
 
-console.log('1234:', (1234).format());           // "1,234"
-console.log('1234:', 1234..format());           // "1,234"
-console.log('12345:', 12345..format(2));         // "12,345.00"
-console.log('123456.7:', 123456.7.format(3, 2));    // "12,34,56.700"
-console.log('123456.789:', 123456.789.format(2, 4));  // "12,3456.79"
+
+/**
+ * Number.prototype.format2(fix, seperator, seperator2)
+ * 
+ * @param {number} fix 포맷 적용할 숫자
+ * @param {string} separator 소수점 구분자
+ * @param {string} separator2 천 단위 구분자
+ * 
+ * usage:
+ *  - (123456).format2(0) // "123,456"
+ *  - (123456).format2(2) // "123,456.00"
+ *  - (123456).format2(2, '|') // "123,456|00"
+ *  - (123456).format2(2, null, '|') // "123|456.00"
+ */
+Number.prototype.format2 = function(c, d, t) {
+	var n = this, 
+	c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "." : d, 
+	t = t == undefined ? "," : t, s = n < 0 ? "-" : "", 
+	i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), j = (j = i.length) > 3 ? j % 3 : 0;
+	
+	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t)
+			+ (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
+console.log('format2():')
+console.log('- format2():', (123456).format2(0)); // "123,456"
+console.log('- format2():', (123456).format2(2)); // "123,456.00"
+console.log('- format2():', (123456.789).format2(4)); // 123,456.7890
+console.log('- format2():', (123456).format2(2, '|')); // "123,456|00"
+console.log('- format2():', (123456).format2(2, null, '|')); // "123|456.00"
+console.log('\n');
+
+/**
+ * Number.prototype.format3(n, x, s, c)
+ * 
+ * @param {number} n length of decimal
+ * @param {number} x length of whole part
+ * @param {object} s sections delimiter
+ * @param {object} c decimal delimiter
+ */
+Number.prototype.format3 = function(n, x, s, c) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+        num = this.toFixed(Math.max(0, ~~n));
+
+    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+};
+console.log('format3():')
+console.log('- 12345678.9:', (12345678.9).format3(2, 3, '.', ','));  // "12.345.678,90"
+console.log('- 123456.789:', (123456.789).format3(4, 4, ' ', ':'));  // "12 3456:7890"
+console.log('- 12345678.9:', (12345678.9).format3(0, 3, '-'));       // "12-345-679"
+console.log('\n');
