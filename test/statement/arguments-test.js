925b5b6159177cacdac5bf1656ebfb37aa117273
noritersand <noritersand@gmail.com>
2021-03-07 13:45:37 +0900

    Add a test suite

--- test/statement/arguments-test.js
+++ test/statement/arguments-test.js
@@ -1,18 +1,25 @@
+const assert = require('assert');
+
 const { log } = console;
 
 function invokeCallback(callback) {
-  log(arguments.length);
+  // log(arguments.length);
   var args = [];
   for (var i = 1; i < arguments.length; i++) {
-    log('i:', i, 'arguments[i]:', arguments[i])
+    // log('i:', i, 'arguments[i]:', arguments[i])
     args.push(arguments[i]);
   }
-  callback.apply(null, args);
+  return callback.apply(null, args);
 }
-
-invokeCallback(fn, 'PRE', 'AA');
 
 function fn(mode, abc) {
-  log('mode:', mode);
-  log('abc:', abc);
+  return { mode, abc };
 }
+
+describe('arguments test suite', function() {
+  it('should be pass', function() {
+    var o = invokeCallback(fn, 'PRE', 'AA');
+    assert.strictEqual(o.mode, 'PRE');
+    assert.strictEqual(o.abc, 'AA');
+  });
+});
