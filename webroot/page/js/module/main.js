a563905960173d63e26bf57f0d837e800b456e49
fixalot <fixalot@outlook.kr>
2021-12-20 18:54:58 +0900

    ES2015 module 테스트 중
    Signed-off-by: fixalot <fixalot@outlook.kr>

---
+++ webroot/page/js/module/main.js
@@ -0,0 +1,26 @@
+// // main.js
+// import { a, b, obj } from './module1.js';
+
+// console.log(a); // 1
+// console.log(b); // 2
+// console.log(JSON.stringify(obj)); // {"a":1,"b":"2"}
+
+// // main.js
+// import { c as foot, d as doratman } from './module1.js';
+
+// console.log(foot); // cfoot
+// console.log(doratman); // dorat?
+
+// // main.js
+// import { Newbie } from './module1.js';
+
+// var noob = new Newbie();
+// noob.levelUp(); // I feel stronger.
+
+// import { str1, bar } from './module1.js';
+
+// console.log(str1);
+// console.log(bar);
+
+import fn1 from './module1.js';
+fn1();
---
+++ webroot/page/js/module/module2.js
@@ -0,0 +1,22 @@
+// // module1.js
+// export const a = 1, b = '2';
+// export let obj = { a, b };
+
+// // module1.js
+// var c = 'cfoot';
+// var d = 'dorat?';
+
+// export { c, d };
+
+// // module1.js
+// export class Newbie {
+//   levelUp() {
+//     console.log('I feel stronger.');
+//   }
+// }
+
+// export const { str1, str2: bar } = { str1: 'abc', str2: 'def'};
+
+export function fn2() {
+  console.log('hi');
+}
