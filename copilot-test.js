b49bbd3f479d3d8f04bcaef970ced7daa5ad4f48
noritersand <noritersand@gmail.com>
2022-02-06 01:58:44 +0900

    Add copilot-test.js

--- copilot-test.js
+++ copilot-test.js
@@ -1,11 +1,25 @@
-// 구구단(2~9단)을 출력
-function gugudan() {
-    var result = '';
-    for (var i = 2; i <= 9; i++) {
-        for (var j = 1; j <= 9; j++) {
-            result += i + ' * ' + j + ' = ' + i * j + '\n';
-        }
-    }
-    return result;
+/**
+ * 단축키:
+ * - 코파일럿 발동: alt + \
+ * - 코파일럿 제안 선택: tab
+ * - 자동 완성 제안 창 보기: ctrl + enter
+ * - 자동 완성 제안 창 닫기: ctrl + w
+ */
+
+/**
+ * 구구단을 출력
+ * @param {number} n 
+ */
+function multiplicationTable(n) {
+  for (let i = 1; i <= 9; i++) {
+    console.log(n + " * " + i + " = " + n * i);
+  }
 }
-console.log(gugudan());
+multiplicationTable(2);
+multiplicationTable(3);
+multiplicationTable(4);
+multiplicationTable(5);
+multiplicationTable(6);
+multiplicationTable(7);
+multiplicationTable(8);
+multiplicationTable(9);
