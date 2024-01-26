fd84e4474d482ba65e53100d7e59487cbdf8b2ef
fixalot <fixalot@anyssign.com>
2024-01-26 10:55:31 +0900

    Update

--- src/live-server/test-bed1.js
+++ src/live-server/test-bed1.js
@@ -1,23 +1,9 @@
-const { log } = console;
+const {log} = console;
 
-function getSomeWithVar() {
-  var fn;
-  for (var i = 0; i < 1; ++i) {
-    fn = () => {
-      console.log(i);
-    }
-  }
-  return fn;
-}
-getSomeWithVar()(); // 1
+var rxp = RegExp(/\d+/);
+log(rxp.exec('1234')); // Array [ "1234" ]
+log(rxp.exec('abc')); // null
 
-function getSomeWithLet() {
-  var fn;
-  for (let i = 0; i < 1; ++i) {
-    fn = () => {
-      console.log(i);
-    }
-  }
-  return fn;
-}
-getSomeWithLet()(); // 0
+var rxp2 = RegExp(/\d*/);
+log(rxp2.exec('1234')); // Array [ "1234" ]
+log(rxp2.exec('abc')); // Array [ "" ] // 빈 문자열 배열이 반환되는 이유는 0개 이상의 숫자를 의미하는 정규식 때문이다
