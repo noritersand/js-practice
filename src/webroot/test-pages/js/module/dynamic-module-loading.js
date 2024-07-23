7a4de819278471aa2d22e0eaebd2dabc3478cc85
fixalot <fixalot@a-na.me>
2024-07-23 12:02:14 +0900

    Update

--- src/webroot/test-pages/js/module/dynamic-module-loading.js
+++ src/webroot/test-pages/js/module/dynamic-module-loading.js
@@ -9,6 +9,8 @@ let btn = document.querySelector('button#btn');
 
 btn.addEventListener('click', () => {
   import('./messages.js').then((module) => {
+    console.log('module:', module);
+    console.log('module.default:', module.default);
     console.log('module.default.wassup:', module.default.wassup);
     console.log('module.foo.bar:', module.foo.bar);
   });
