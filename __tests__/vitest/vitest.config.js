3a259162f2b649c82f37ad3cb16cfa8f1ef44832
fix-a-lot <fixalot@outlook.kr>
2025-04-02 11:40:27 +0900

    feat: COPYME_LIGHT 추가하고 Vitest 제외 설정

--- __tests__/vitest/vitest.config.js
+++ __tests__/vitest/vitest.config.js
@@ -3,6 +3,7 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
   test: {
     watch: true,
-    root: '__tests__/vitest'
+    root: '__tests__/vitest',
+    exclude: ['COPYME*.test.js']
   }
 });
