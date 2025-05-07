1c44302c308b100de34e8e6dbcf85099f11b2023
fix-a-lot <fixalot@outlook.kr>
2025-05-07 14:27:50 +0900

    feat: 404 처리

--- server.js
+++ server.js
@@ -97,3 +97,12 @@ app.post('/uncategorized/*.data', (req, res) => {
 app.post('/*.html', (req, res) => {
   res.json(req.body);
 });
+
+// 404 처리. 라우트 설정 가장 마지막에 있어야 함
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: `요청한 경로가 존재하지 않습니다: ${req.originalUrl}`,
+    code: -99,
+  });
+});
